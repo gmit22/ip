c506e6aec4d797e9c9e1c09c5aba17b1ca27665b
DESKTOP-7Q3MLGS\gunit <gunitmittal@gmail.com>
2020-08-18 16:41:50 +0530

    class created for task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -46,4 +46,26 @@ public class Duke {
         System.out.println(indent + "Bye! Hope to see you again soon!");
         System.out.println(indent + "------------");
     }
+
+    public static class Task {
+
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+
+            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        }
+
+        public void markAsDone() {
+
+            this.isDone = true;
+        }
+    }
 }
