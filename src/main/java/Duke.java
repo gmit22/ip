5b325ee6e51aac29d74737402aa317bf42d19b46
DESKTOP-7Q3MLGS\gunit <gunitmittal@gmail.com>
2020-08-18 16:09:09 +0530

    Level-1 updated

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 
         greet();
     }
 
-    public static void greet(){
+    public static void greet() {
 
-        System.out.println("------------");
-        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
-        System.out.println("------------");
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println("------------");
+        String str;
+        String indent = "    ";
+
+        System.out.println(indent + "------------");
+        System.out.println(indent + "Hello! I'm Duke \n    What can I do for you?");
+        System.out.println(indent + "------------");
+
+        Scanner in = new Scanner(System.in);
+        str = in.nextLine();
+
+        while (!str.equals("bye")) {
+
+            System.out.println(indent + "------------");
+            System.out.println(indent + "added: " + str);
+            System.out.println(indent + "------------");
+            str = in.nextLine();
+        }
+
+        System.out.println(indent + "------------");
+        System.out.println(indent + "Bye! Hope to see you again soon!");
+        System.out.println(indent + "------------");
     }
 }
